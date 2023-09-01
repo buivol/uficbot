738561f9a8b32a6e4a54e15306f34b8ef3c9b997
tryakin <sasha@tryakin.ru>
2023-09-01 20:00:14 +0300

    add logger

--- init.php
+++ init.php
@@ -14,7 +14,7 @@ use SergiX44\Nutgram\RunningMode\Webhook;
 use Yiisoft\Cache\ArrayCache;
 use \Yiisoft\Db\Cache\SchemaCache;
 use \Yiisoft\Db\Mysql\Connection;
-
+use \SergiX44\Nutgram\Configuration;
 
 // Connection.
 $arrayCache = new ArrayCache();
@@ -22,7 +22,7 @@ $schemaCache = new SchemaCache($arrayCache);
 $db = new Connection($config['driver'], $schemaCache);
 
 
-$tg = new Nutgram($config['telegram']['token']);
+$tg = new Nutgram($config['telegram']['token'], new Configuration(logger: \SergiX44\Nutgram\Logger\ConsoleLogger::class));
 $tg->setRunningMode(Webhook::class);
 //TODO: Сделать чтобы ставился один раз, а не при каждом вызове
 //$tg->setWebhook($config['telegram']['webhookUrl']);
